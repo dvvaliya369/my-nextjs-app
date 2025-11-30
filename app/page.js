412e264462671499583ca33ca19864c96958ee5d
dvvaliya369 <dvvaliya369@gmail.com>
2025-12-01 02:34:52 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
+          <h1>Lets get started</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
